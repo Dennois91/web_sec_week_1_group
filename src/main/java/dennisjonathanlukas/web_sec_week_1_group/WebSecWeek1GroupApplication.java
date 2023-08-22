2979ccf8a3980c6686f7029379e29b051e667b9f
Dennois91 <109462612+Dennois91@users.noreply.github.com>
2023-08-22 09:50:09 +0200

    First

---
+++ src/main/java/dennisjonathanlukas/web_sec_week_1_group/WebSecWeek1GroupApplication.java
@@ -0,0 +1,15 @@
+package dennisjonathanlukas.web_sec_week_1_group;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@SpringBootApplication
+public class WebSecWeek1GroupApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebSecWeek1GroupApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dennisjonathanlukas/web_sec_week_1_group/WebSecWeek1GroupApplicationTests.java
@@ -0,0 +1,13 @@
+package dennisjonathanlukas.web_sec_week_1_group;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebSecWeek1GroupApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
