94c896808b1f7fc56ab1bc18a9d7d713cceab964
Lukas Chyle <lukas_chyle@hotmail.com>
2023-08-22 13:15:58 +0200

    Api findById

--- src/main/java/dennisjonathanlukas/web_sec_week_1_group/controllers/AccountController.java
+++ src/main/java/dennisjonathanlukas/web_sec_week_1_group/controllers/AccountController.java
@@ -2,7 +2,9 @@ package dennisjonathanlukas.web_sec_week_1_group.controllers;
 
 import dennisjonathanlukas.web_sec_week_1_group.model.Account;
 import dennisjonathanlukas.web_sec_week_1_group.repositories.AccountRepository;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,5 +24,12 @@ public class AccountController {
     @GetMapping
     public List<Account> getAccounts() {
         return accountRepository.findAll();
+    }
+
+    @RequestMapping("/{id}")
+    public Account getPurchaseById(@PathVariable Long id) {
+        System.out.println("Fetching id: id");
+        return accountRepository.findById(id).orElseThrow(()
+                -> new EntityNotFoundException("Purchase id: " + id + " not valid"));
     }
 }
